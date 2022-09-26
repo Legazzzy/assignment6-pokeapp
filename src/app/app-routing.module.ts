a3aedf493f3162bc1235a3f8e6559880d53da725
Legazzzy <marius.eriksen13@yahoo.no>
2022-09-26 10:06:16 +0200

    added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { ProfilePage } from './pages/profile/profile.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginPage
+  },
+  {
+    path: "pokemons",
+    component: PokemonCataloguePage
+  },
+  {
+    path: "profile",
+    component: ProfilePage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/pokemon-catalogue/pokemon-catalogue.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-catalogue',
+  templateUrl: './pokemon-catalogue.page.html',
+  styleUrls: ['./pokemon-catalogue.page.scss']
+})
+export class PokemonCataloguePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
