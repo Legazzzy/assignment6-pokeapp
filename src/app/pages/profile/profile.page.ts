41cbcf7f0c3e952de566b04b1c10a4adb2946210
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-09-28 14:01:17 +0200

    Implemented profile page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon.model';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly userService: UserService
+  ) { }
+
+  private _trainerName: string | undefined = this.userService.user?.username;
+  private _pokemons: Pokemon[] | undefined = this.userService.user?.pokemon;
+
+  get trainerName(): string {
+    return this._trainerName!;
+  }
+
+  set trainerName(n: string) {
+    this._trainerName = n;
+  }
+
+
+  get pokemons(): Pokemon[] {
+    return this._pokemons!;
+  }
+
+  set pokemons(p: Pokemon[]) {
+    this._pokemons = p;
+  }
+
+  public handleRemove(pokemonName:string): void {
+    let newPokemons:Pokemon[] | undefined = [];
+    this.pokemons.forEach(pokemon => {
+      if (pokemon.name !== pokemonName) {
+        newPokemons?.push(pokemon)
+      }
+    })
+    this._pokemons = newPokemons!;
+    this.userService.updatePokemons(this._pokemons);
+  }
+
+  public removePokemon(pokemon: Pokemon): void {
+
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { StorageKeys } from '../enums/storage-keys.enum';
+import { Pokemon } from '../models/pokemon.model';
 import { User } from '../models/user.model';
 import { StorageUtil } from '../utils/storage.util';
 
@@ -17,6 +18,15 @@ export class UserService {
   set user(user: User | undefined) {
     StorageUtil.storageSave<User>(StorageKeys.User, user!);
     this._user = user;
+  }
+
+  public updatePokemons(pokemons: Pokemon[]): void {
+    let newUser:User | undefined = {
+      id: this._user?.id!,
+      username: this._user?.username!,
+      pokemon: pokemons
+    }
+    this.user = newUser;
   }
 
   constructor() { 
