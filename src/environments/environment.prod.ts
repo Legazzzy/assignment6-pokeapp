6a59a7510f7dcae581f9f3967c016a8c77b332ac
Legazzzy <marius.eriksen13@yahoo.no>
2022-09-28 11:28:59 +0200

    added server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   apiUsers: "https://assignment2-sign-translator.herokuapp.com/trainers",
   apiPokemons: "https://pokeapi.co/api/v2/pokemon",
+  apiKey: "chAFe94loBrDpnPps1dbnhPEYrMVVvHlmjkC3eTCBVH5gUim9Yquv7XdkS1Jvrkn"
 };
